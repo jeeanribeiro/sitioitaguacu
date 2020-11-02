4db5adbc77c8898d29bcec4db9ee9c020dbfa214
Jean Ribeiro <jeanmendesrib@gmail.com>
2020-11-02 16:18:29 -0300

    Change whatsapp number

--- src/components/Whatsapp.svelte
+++ src/components/Whatsapp.svelte
@@ -10,6 +10,6 @@
 	}
 </style>
 
-<a href="https://api.whatsapp.com/send?phone=5511975905649&text=Ol%C3%A1!+estou+entrando+em+contato+atrav%C3%A9s+do+site+do+S%C3%ADtio+Itagua%C3%A7%C3%BA&source&data&app_absent&lang=pt_br" class="whatsapp">
+<a href="https://api.whatsapp.com/send?phone=5511910371018&text=Ol%C3%A1!+estou+entrando+em+contato+atrav%C3%A9s+do+site+do+S%C3%ADtio+Itagua%C3%A7%C3%BA&source&data&app_absent&lang=pt_br" class="whatsapp">
 	<img src="assets/whatsapp-icon.svg" alt="Whatsapp" />
 </a>
