f50896385a323a19f0947455b395c4319a5632d6
Jean Ribeiro <jeanmendesrib@gmail.com>
2022-05-26 09:01:14 -0300

    Updates WhatsApp number and year in footer (#13)
    * Update Whatsapp.svelte
    
    * Update Footer.svelte

--- src/components/Whatsapp.svelte
+++ src/components/Whatsapp.svelte
@@ -10,6 +10,6 @@
 	}
 </style>
 
-<a href="https://api.whatsapp.com/send?phone=5511933767244&text=Ol%C3%A1!+estou+entrando+em+contato+atrav%C3%A9s+do+site+do+S%C3%ADtio+Itagua%C3%A7%C3%BA&source&data&app_absent&lang=pt_br" class="whatsapp">
+<a href="https://api.whatsapp.com/send?phone=5511911100667&text=Ol%C3%A1!+estou+entrando+em+contato+atrav%C3%A9s+do+site+do+S%C3%ADtio+Itagua%C3%A7%C3%BA&source&data&app_absent&lang=pt_br" class="whatsapp">
 	<img src="assets/whatsapp-icon.svg" alt="Whatsapp" />
 </a>
